a1ead2af52b9cb6098bc11ac8fec180eb75cb119
Manas Mishra <mmanas@vmware.com>
2021-09-08 12:30:30 +0530

    React copy paste build working

---
+++ react-copy-paste/src/Root.client.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Root() {
+    return (
+        <div>
+            HELLO
+        </div>
+    )
+}
+
+export default Root
---
+++ react-copy-paste/src/index.client.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import React from 'react';
+import Root from './Root.client'
+
+ReactDOM.render(<Root/>, document.getElementById("root"))
